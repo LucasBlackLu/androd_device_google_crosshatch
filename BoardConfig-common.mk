43c658e67662c515bf5a54d9bcbc286395e4bf6e
lucasblacklu <lucasblack997@gmail.com>
2022-12-07 18:37:49 +0000

    BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -20,6 +20,8 @@ include build/make/target/board/BoardConfigPixelCommon.mk
 TARGET_BOARD_PLATFORM := sdm845
 TARGET_BOARD_INFO_FILE := device/google/crosshatch/board-info.txt
 USES_DEVICE_GOOGLE_B1C1 := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
